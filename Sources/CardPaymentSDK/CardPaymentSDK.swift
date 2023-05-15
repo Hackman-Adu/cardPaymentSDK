aec79615465c249a5677ab667968e587cc48b74f
Hackman Adu Gyamfi <hgyamfi@IM-24-B1-008.local>
2023-05-15 13:14:32 +0000

    added card details

--- Sources/CardPaymentSDK/CardPaymentSDK.swift
+++ Sources/CardPaymentSDK/CardPaymentSDK.swift
@@ -3,18 +3,25 @@
 
 public struct CardPaymentSDK {
 
-    public static func configureUser(cardnumber:String){
+    public static func configureCard(cardnumber:String, cardholder:String){
         cardNumber = cardnumber
+        cardHolder = cardholder
     }
     
     private static var cardNumber:String?
     
-    public static func printCardNumber(){
+    private static var cardHolder:String?
+    
+    public static func getCardInfo(){
         
         guard let number = cardNumber else {
             print("Please enter card number")
             return
         }
-        print("THIS IS THE CARD NUMBER \(number)")
+        guard let name = cardHolder else {
+            print("Please enter card holder name")
+            return
+        }
+        print("THIS IS THE CARD INFORMATION/n Card Number:\(number)/n Card Holder Name:\(name)")
     }
 }
