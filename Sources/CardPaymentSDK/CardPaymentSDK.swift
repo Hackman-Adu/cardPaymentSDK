7fd304a0497a0bc47d55fb70767f4e60080e3d05
Hackman Adu Gyamfi <hgyamfi@IM-24-B1-008.local>
2023-05-22 11:33:13 +0000

    created webview

--- Sources/CardPaymentSDK/CardPaymentSDK.swift
+++ Sources/CardPaymentSDK/CardPaymentSDK.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import UIKit
+import WebKit
 
 
 public struct CardPaymentSDK {
@@ -29,4 +30,35 @@ public struct CardPaymentSDK {
         let message = "Card Number: \(number)\n Card Holder Name: \(name)"
         alert.showAlert("Card Information", message)
     }
+    
+    public static func showCardPaymentView(){
+        var controller = CardPaymentView()
+        viewController?.present(controller, animated: true)
+    }
 }
+
+
+public class CardPaymentView:UIViewController, WKScriptMessageHandler{
+    
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+       print("THIS IS WHERE CALL BACK WILL BE HANDLED")
+    }
+    
+    
+    public var webView:WKWebView!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        let config = WKWebViewConfiguration()
+        config.userContentController.add(self,name: "web_content_hanlder")
+        let webview = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), configuration:config)
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        self.view.addSubview(webView)
+        if let url = URL(string: "https://www.google.com"){
+            webView.load(.init(url: url))
+        }
+    }
+}
+
