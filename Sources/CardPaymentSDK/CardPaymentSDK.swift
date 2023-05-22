7e34e85d3ae2c5e20a44f828a99b3d030ddc6bca
Hackman Adu Gyamfi <hgyamfi@IM-24-B1-008.local>
2023-05-22 12:45:43 +0000

    resolved nav issue

--- Sources/CardPaymentSDK/CardPaymentSDK.swift
+++ Sources/CardPaymentSDK/CardPaymentSDK.swift
@@ -34,6 +34,7 @@ public struct CardPaymentSDK {
     public static func showCardPaymentView(){
         var controller = CardPaymentView()
         var navController = UINavigationController(rootViewController: controller)
+        navController.modalPresentationStyle = .fullScreen
         navController.present(controller, animated: true)
     }
 }
